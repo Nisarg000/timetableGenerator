584e499cc1da33c93b107bb1959609fef0422043
Nisarg000 <nisargjhatakia94@gmail.com>
2022-02-10 09:20:01 +0530

    A few formatting

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const menuButton = document.querySelector('.main-container nav .menu-button');
-const closeButton = document.querySelector('.mobile-menu-items .close-button');
-const mainContainer = document.querySelector('.main-container');
+const menuButton = document.querySelector(".main-container nav .menu-button");
+const closeButton = document.querySelector(".mobile-menu-items .close-button");
+const mainContainer = document.querySelector(".main-container");
 
-menuButton.addEventListener('click', () => { 
-    mainContainer.classList.add('active');
+menuButton.addEventListener("click", () => { 
+    mainContainer.classList.add("active");
 });
 
-closeButton.addEventListener('click', () => {
-    mainContainer.classList.remove('active');
+closeButton.addEventListener("click", () => {
+    mainContainer.classList.remove("active");
 });
